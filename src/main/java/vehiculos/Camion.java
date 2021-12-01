e147def7d48b1d6a16946763d34fbc697faba05b
MILK <rubencdlm1@hotmail.com>
2021-11-30 20:17:27 -0500

    extra cum

--- src/main/java/vehiculos/Camion.java
+++ src/main/java/vehiculos/Camion.java
@@ -1,32 +1,28 @@
 package vehiculos;
 
-public class Camion extends Vehiculo{
-
-	private int ejes; 
-	private static int camiones; 
-
+public class Camion extends Vehiculo {	
+	private int ejes;
+	private static int camiones;
+	
 	public Camion() {
-		camiones++;	
+		camiones++;
 	}
 	
 	public Camion(String placa, String nombre, int precio, int peso,Fabricante fabricante, int ejes) {
-		super(placa, 2, 80, nombre, precio, peso, "4x2", fabricante);
+		super(placa, 2, 80, nombre, precio, peso, "4X2", fabricante);
 		this.ejes = ejes;
-		camiones++;		
-		
+		camiones++;
 	}
 
-	
 	public int getEjes() {
 		return ejes;
 	}
 	
 	public void setEjes(int ejes) {
-		this.ejes = ejes;	
+		this.ejes = ejes;
 	}
 	
 	public static int getCantidadCamiones() {
 		return camiones;
 	}
-	
 }
--- src/main/java/vehiculos/Camioneta.java
+++ src/main/java/vehiculos/Camioneta.java
@@ -1,24 +1,20 @@
 package vehiculos;
 
 public class Camioneta extends Vehiculo {
+	private boolean volco;
+	private static int camionetas;
 
-	private Boolean volco;
-	private static int camionetas; 
-	
-	
-	
-	
 	public Camioneta() {
 		camionetas++;
 	}
 	
 	public Camioneta(String placa, int puertas, String nombre, int precio, int peso, Fabricante fabricante,boolean volco) {
-		super(placa, puertas, 90, nombre, precio, peso, "4x4", fabricante);
+		super(placa, puertas, 90, nombre, precio, peso, "4X4", fabricante);
 		this.volco = volco;
 		camionetas++;
+	}
 	
-	}	
-
+	
 	public boolean isVolco() {
 		return volco;
 	}
@@ -26,10 +22,8 @@ public class Camioneta extends Vehiculo {
 	public void setVolco(boolean volco) {
 		this.volco = volco;
 	}
-
+	
 	public static int getCantidadCamionetas() {
 		return camionetas;
 	}
-
 }
-
