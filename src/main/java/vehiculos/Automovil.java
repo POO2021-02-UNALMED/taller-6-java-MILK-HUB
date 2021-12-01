bed6adcf12702af2dab794f541650337ea0d9258
MILK <rubencdlm1@hotmail.com>
2021-11-30 19:53:26 -0500

    update fix

--- src/main/java/vehiculos/Automovil.java
+++ src/main/java/vehiculos/Automovil.java
@@ -6,9 +6,9 @@ public class Automovil extends Vehiculo{
 	private static int automoviles = 0;
 	
 	
-	public Automovil(String placa, String nombre, int precio, int peso, Fabricante fabricante){
-		super(placa, 4, 100, nombre, precio, peso "FWD", fabricante);
-		this.puestos = puestos; 
+	public Automovil(String placa, String nombre, int precio, float peso, Fabricante fabricante, int puestos) {
+		super(placa, 4, 100, nombre, precio, peso, "FWD", fabricante);
+		this.puestos = puestos;
 		automoviles++;
 	}
 	
